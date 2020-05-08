d495e218215c947fbb617900a047741c03652ec7
Alexis Teh <wenhui.teh23@gmail.com>
2020-05-08 20:16:15 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+  a = num1 + num2 
+  return a 
 end
 
 def subtraction(num1, num2)
-
+  s = num1 - num2 
+  return s 
 end
 
 def division(num1, num2)
-
+  d = num2 / num1 
+  return d
 end
 
 def multiplication(num1, num2)
-
+  m = num1 * num2 
+  return m 
 end
 
 def modulo(num1, num2)
-
+  m = num1 % num2 
+  return m 
 end
 
 def square_root(num)
-
+  
 end
